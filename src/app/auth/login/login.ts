0e2298c8694d73cc1821287c56c05e61c597f05e
ManuelGordillo <manuelgordillodeveloper@gmail.com>
2026-06-17 22:54:43 -0500

    Login con validadores y conexion correcta a la base de Datos

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -1,10 +1,59 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Login {}
+export class Login {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  private fb = inject(FormBuilder);
+
+  loginForm = this.fb.group({
+    usuario: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(12)]],
+
+    password: ['', [Validators.required, Validators.minLength(8)]],
+  });
+
+  login(): void {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    const data = {
+      nombreUsuario: this.loginForm.value.usuario,
+      contrasena: this.loginForm.value.password,
+    };
+
+    this.authService.login(data).subscribe({
+      next: (response: any) => {
+        localStorage.setItem('token', response.jwt);
+        localStorage.setItem('rol', response.rol);
+
+        switch (response.rol) {
+          case 'ADMINISTRADOR':
+            this.router.navigate(['/dashboard-administrador']);
+            break;
+
+          case 'VENDEDOR':
+            this.router.navigate(['/dashboard-vendedor']);
+            break;
+
+          case 'CLIENTE':
+            this.router.navigate(['/dashboard-cliente']);
+            break;
+        }
+      },
+
+      error: () => {
+        alert('Usuario o contraseña incorrectos');
+      },
+    });
+  }
+}
--- src/app/auth/registrar-cliente/registrar-cliente.ts
+++ src/app/auth/registrar-cliente/registrar-cliente.ts
@@ -1,9 +1,69 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+  AbstractControl,
+  ValidationErrors,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-registrar-cliente',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './registrar-cliente.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RegistrarCliente {}
+export class RegistrarCliente {
+  private fb = inject(FormBuilder);
+
+  registerForm: FormGroup = this.fb.group(
+    {
+      nombres: ['', Validators.required],
+      apellidos: ['', Validators.required],
+      tipoDocumento: ['', Validators.required],
+      numeroDocumento: ['', Validators.required],
+
+      correo: ['', [Validators.required, Validators.email]],
+
+      telefono: ['', Validators.required],
+
+      direccion: ['', Validators.required],
+
+      usuario: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(12)]],
+
+      password: ['', [Validators.required, Validators.minLength(8)]],
+
+      confirmarPassword: ['', Validators.required],
+    },
+    {
+      validators: this.passwordMatchValidator,
+    },
+  );
+
+  passwordMatchValidator(control: AbstractControl): ValidationErrors | null {
+    const password = control.get('password')?.value;
+    const confirmarPassword = control.get('confirmarPassword')?.value;
+
+    if (password !== confirmarPassword) {
+      return { passwordMismatch: true };
+    }
+
+    return null;
+  }
+
+  registrar(): void {
+    if (this.registerForm.invalid) {
+      this.registerForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.registerForm.value);
+
+    // Aquí llamas tu servicio
+  }
+
+  get f() {
+    return this.registerForm.controls;
+  }
+}
