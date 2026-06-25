d6388ef532b03ab2cab95721479823d30c4473af
ManuelGordillo <manuelgordillodeveloper@gmail.com>
2026-06-24 22:48:39 -0500

    Corrigiendo nombres en las clases

--- src/app/pages/home/pagina-principal/pagina-principal.ts
+++ src/app/pages/home/pagina-principal/pagina-principal.ts
@@ -1,12 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { Navbar } from '../../../shared/components/navbar/navbar';
 import { Footer } from '../../../shared/components/footer/footer';
 import { Testimonios } from './testimonios/testimonios';
 
 @Component({
   selector: 'app-pagina-principal',
-  imports: [RouterLink, Navbar, Footer, Testimonios],
+  imports: [Navbar, Footer, Testimonios],
   templateUrl: './pagina-principal.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
