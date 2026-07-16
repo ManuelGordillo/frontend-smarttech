83de6001c7ef7fa25b97384c8b17794aa7405c9c
ManuelGordillo <manuelgordillodeveloper@gmail.com>
2026-07-15 21:08:37 -0500

    Dashboard administrador ya muestra el usuario en linea

--- src/app/pages/administrador/components/side-menu/side-menu-header/side-menu-header.ts
+++ src/app/pages/administrador/components/side-menu/side-menu-header/side-menu-header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -6,4 +6,24 @@ import { RouterLink } from '@angular/router';
   imports: [RouterLink],
   templateUrl: './side-menu-header.html',
 })
-export class SideMenuHeader {}
+export class SideMenuHeader {
+  nombreAdministrador = signal<string>('');
+  rol = signal<string>('');
+
+  ngOnInit(): void {
+    const rol = localStorage.getItem('rol') || 'ADMINISTRADOR';
+    this.rol.set(rol);
+
+    const nombre =
+      localStorage.getItem('nombre') ||
+      localStorage.getItem('username') ||
+      localStorage.getItem('nombreUsuario') ||
+      localStorage.getItem('nombreVendedor') ||
+      'Usuario';
+
+    console.log('📌 Rol:', rol);
+    console.log('📌 Nombre:', nombre);
+
+    this.nombreAdministrador.set(nombre);
+  }
+}
